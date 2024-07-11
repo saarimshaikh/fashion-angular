3ee7df83fd187eb6b20aa2797b69d03527a6960a
saarimshaikh <saarimshaikhk1234@gmail.com>
2024-07-11 12:13:20 +0530

    made changes

--- src/app/pipes/price.pipe.ts
+++ src/app/pipes/price.pipe.ts
@@ -7,6 +7,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PricePipe implements PipeTransform {
   // Used to append a dollar sign to the price
   transform(value: unknown, ...args: unknown[]): unknown {
-    return `${value}$`;
+    return `${value}Rs`;
   }
 }
